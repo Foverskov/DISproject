f61f2a24d3edaa2caba00e39290d45e151be40f3
Foverskov <114136233+Foverskov@users.noreply.github.com>
2023-05-18 13:03:35 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2
+
+
+app = Flask(__name__)
+
+# Connect to the database
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="postgres"
+)
+
+@app.route('/')
+def index():
+    # Connect to the database
+    conn = psycopg2.connect(database="postgres",
+                            user="postgres",
+                            password="postgres",
+                            host="localhost", port="5433")
+  
+    # create a cursor
+    cur = conn.cursor()
+  
+    # Select all products from the table
+    cur.execute('''SELECT * FROM products''')
+  
+    # Fetch the data
+    data = cur.fetchall()
+  
+    # close the cursor and connection
+    cur.close()
+    conn.close()
+  
+    return render_template('index.html', data=data)
