1311eb3769b118cb589bba8bdaa34d52ed8d0e7d
Elmegaard1901 <mathias.elmegaard@gmail.com>
2023-06-08 10:44:32 +0200

    forside, html-fil til medlemmer

--- app.py
+++ app.py
@@ -25,16 +25,6 @@ def index():
     page = render_template('index.html')
     return page
 
-    # """
-    # <h1>Velkommen til vores side</h1>
-    # <p>Her kan du se vores medlemmer, ansatte, faciliteter, hold, medlemskaber og bookinger</p>
-    # <a href="/medlemmer">Medlemmer</a>
-    # <a href="/hold">Holdstruktur</a>
-    # <a href="/faciliteter">Faciliteter</a>
-    # <a href="/mail">Beskeder</a>
-    # """
-
-
 """
 Medlemmer
 Data: 
